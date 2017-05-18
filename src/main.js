ebf6fba1480cb2ed9049406fe5bb5e0de396de6e
周尔耕 <zhou.ergeng@trs.com.cn>
2017-05-18 22:04:05 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/css/bootstrap.min.css'
-import './assets/css/cover.css'
+import './assets/css/base.css'
+import './assets/css/font.css'
+import './assets/css/common.css'
 
 new Vue({
   el: '#app',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,42 @@
 <template>
-    
+    <div>
+        <div class="header">
+            <div class="logo"><img src="../assets/images/logo.png" /></div>
+            <div class="nav">
+                <div class="box">
+                    <a class="animate-link" href="">Home</a>
+                    <a class="animate-link" href="">Article</a>
+                    <a class="animate-link" href="">About</a>
+                </div>
+            </div>
+        </div>
+        <div class="main">
+            <ul>
+                <li>
+                    <h3>2017.5.18</h3>
+                    <h2><a>Welcome to gblog</a></h2>
+                </li>
+                <li>
+                    <h3>2017.5.18</h3>
+                    <h2><a>你好，未来</a></h2>
+                </li>
+                <li>
+                    <h3>2017.5.18</h3>
+                    <h2><a>Welcome to gblog</a></h2>
+                </li>
+                <li>
+                    <h3>2017.5.18</h3>
+                    <h2><a>你好，未来</a></h2>
+                </li>
+                <li>
+                    <h3>2017.5.18</h3>
+                    <h2><a>Welcome to gblog</a></h2>
+                </li>
+                <li>
+                    <h3>2017.5.18</h3>
+                    <h2><a>你好，未来</a></h2>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
