30fe2af163ae60c09968f9a9bb8643013e724179
周尔耕 <zhou.ergeng@trs.com.cn>
2017-05-19 17:47:45 +0800

    update

--- src/views/layout/pager.vue
+++ src/views/layout/pager.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="pager">
-        <span class="prev n"><</span>
-        <span class="num m">1</span>
-        <span class="num">2</span>
-        <span class="num">3</span>
-        <span class="num">4</span>
-        <span class="num">5</span>
-        <span class="next">></span>
+        <span class="prev n animate-link"><<</span>
+        <span class="num m animate-link">1</span>
+        <span class="num animate-link">2</span>
+        <span class="num animate-link">3</span>
+        <span class="num animate-link">4</span>
+        <span class="num animate-link">5</span>
+        <span class="next animate-link">>></span>
     </div>
 </template>
 <script>
